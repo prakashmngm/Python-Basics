b7f44cad30119dc79b465fdf1666df07dbfadec2
Suryaprakash Mangam <suryaprakash@kd-b7d851cc-26fe-11b2-a85c-ae988e5273a5.ibm.com>
2022-02-01 12:48:00 +0530

    commiting

---
+++ 08_Linkedlist_Operations/08_doublyLinkedList.py
@@ -0,0 +1,79 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.nextPtr = None
+        self.prevPtr = None
+class DLL:
+    def __init__(self):
+        self.head = None
+        
+    def append(self,nodeobj):
+        if(self.head == None):
+            self.head = nodeobj
+        else:
+            tempPtr = self.head
+            while(tempPtr.nextPtr != None):
+                tempPtr = tempPtr.nextPtr
+            tempPtr.nextPtr = nodeobj
+            nodeobj.prevPtr = tempPtr
+            
+    def printDLL(self):
+        tempPtr = self.head
+        while(tempPtr != None):
+            print(str(tempPtr.data)+'->',end = '')
+            tempPtr = tempPtr.nextPtr
+        print()
+    
+    def printDLLReverse(self):
+        tempPtr = self.head
+        while(tempPtr.nextPtr != None):
+            tempPtr = tempPtr.nextPtr
+        while(tempPtr != None):
+            print(str(tempPtr.data)+'->',end = '')
+            tempPtr = tempPtr.prevPtr
+        print()
+    
+    def createDLLfromList(self,lst):
+        for ele in lst:
+            node = Node(ele)
+            self.append(node)
+            
+    def insert(self,index,nodeobj):
+        tempPtr = self.head
+        start = 0
+        while(start < index-1):
+            tempPtr = tempPtr.nextPtr
+            start = start+1
+        nodeobj.nextPtr = tempPtr.nextPtr
+        nodeobj.prevPtr = tempPtr
+        tempPtr.nextPtr = nodeobj
+        tempPtr = tempPtr.nextPtr.nextPtr
+        if(tempPtr != None):
+            tempPtr.prevPtr = nodeobj
+#--------------------------------------------------------------------
+
+N1 = Node(5)
+myDLL = DLL()
+myDLL.append(N1)
+myDLL.printDLLReverse()
+myDLL.printDLL()
+
+N2 = Node(6)
+myDLL.append(N2)
+myDLL.printDLL()
+myDLL.printDLLReverse()
+
+N3 = Node(7)
+myDLL.append(N3)
+myDLL.printDLL()
+myDLL.printDLLReverse()
+
+myDLL1 = DLL()
+myDLL1.createDLLfromList([11,12,13,18,19,20])
+myDLL1.printDLL()
+myDLL1.printDLLReverse()
+
+N4 = Node(25)
+myDLL1.insert(6,N4)
+myDLL1.printDLL()
+myDLL1.printDLLReverse()
